28c499eaf6230cbd2ea4f02494ed084f29a11fc8
Tobias Sette <contato@tobias.ws>
2018-03-05 16:10:58 -0300

    Creates the necessary images for the project

---
+++ docker/images/utils/init.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+echo "Started at $(date)"
+
+while [[ ! -f package.json ]]; do
+    echo "Unable to locate package.json"
+    sleep 5s
+done
+
+echo "Running npm install"
+npm install
+
+echo "Starting grunt"
+node_modules/grunt/bin/grunt wjs &
+
+echo "Starting sass"
+sass --watch static/sass:public/css
